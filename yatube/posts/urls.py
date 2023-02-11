6b474e96feaf23d19999ba3180e5dd930ef7c43f
Егор Бойко <ego.boiko2018@yandex.ru>
2023-02-11 17:33:49 +0300

    cool commit

---
+++ yatube/posts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name='index'),
+    path('create/', views.post_create, name='post_create'),
+    # Страница сообществ
+    path('group/<slug:slug>/', views.group_posts, name='group_list'),
+    # Профайл пользователя
+    path('profile/<str:username>/', views.profile, name='profile'),
+    # Просмотр записи
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+
+]
---
+++ yatube/posts/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from .models import Group, Post, User
+POST_COUNT_PER_PAGE = 10
+
+
+def index(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, POST_COUNT_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'posts': posts,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:profile', username=request.user)
+    else:
+        form = PostForm()
+    return render(request, 'posts/create_post.html', {"form": form})
+
+
+@login_required
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if post.author != request.user:
+        return redirect('posts:post_detail', post_id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:post_detail', post_id=post_id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/create_post.html',
+                  {"form": form, 'is_edit': True})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group)
+    paginator = Paginator(posts, POST_COUNT_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+        'group': group,
+        'posts': posts,
+        'title': f"Записи сообщества {group}"
+    }
+    return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    post_list = author.posts.all()
+    posts = Post.objects.select_related('author').filter(author=author)
+    posts_amount = posts.count()
+    paginator = Paginator(post_list, POST_COUNT_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'post_list': post_list,
+        'author': author,
+        'posts_amount': posts_amount,
+        'page_obj': page_obj,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    posts_amount = Post.objects.filter(author=post.author).count()
+    # Здесь код запроса к модели и создание словаря контекста
+    context = {
+        'post': post,
+        'posts_amount': posts_amount,
+
+    }
+    return render(request, 'posts/post_detail.html', context)

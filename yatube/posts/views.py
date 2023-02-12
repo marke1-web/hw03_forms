510252d5b275c562ed690c58fe4e1908d53c5406
Егор Бойко <ego.boiko2018@yandex.ru>
2023-02-12 14:04:20 +0300

    cool

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
+from .utils import pagin
 from .models import Group, Post, User
-POST_COUNT_PER_PAGE = 10
 
 
 def index(request):
     posts = Post.objects.all()
-    paginator = Paginator(posts, POST_COUNT_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = pagin(request, posts)
     context = {
-        'posts': posts,
         'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
@@ -20,15 +16,12 @@ def index(request):
 
 @login_required
 def post_create(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', username=request.user)
-    else:
-        form = PostForm()
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect('posts:profile', username=request.user)
     return render(request, 'posts/create_post.html', {"form": form})
 
 
@@ -37,13 +30,10 @@ def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
         return redirect('posts:post_detail', post_id=post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:post_detail', post_id=post_id)
-    else:
-        form = PostForm(instance=post)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post_id=post_id)
     return render(request, 'posts/create_post.html',
                   {"form": form, 'is_edit': True})
 
@@ -51,14 +41,10 @@ def post_edit(request, post_id):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group)
-    paginator = Paginator(posts, POST_COUNT_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = pagin(request, posts)
     context = {
         'page_obj': page_obj,
         'group': group,
-        'posts': posts,
-        'title': f"Записи сообщества {group}"
     }
     return render(request, 'posts/group_list.html', context)
 
@@ -66,15 +52,9 @@ def group_posts(request, slug):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.all()
-    posts = Post.objects.select_related('author').filter(author=author)
-    posts_amount = posts.count()
-    paginator = Paginator(post_list, POST_COUNT_PER_PAGE)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = pagin(request, post_list)
     context = {
-        'post_list': post_list,
         'author': author,
-        'posts_amount': posts_amount,
         'page_obj': page_obj,
     }
     return render(request, 'posts/profile.html', context)
@@ -82,10 +62,7 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    posts_amount = Post.objects.filter(author=post.author).count()
     context = {
         'post': post,
-        'posts_amount': posts_amount,
-
     }
     return render(request, 'posts/post_detail.html', context)
